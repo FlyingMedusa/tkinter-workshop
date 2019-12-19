0012dc3b76a226b2f62f74e67eb9eb9c08fbe522
FlyingMedusa <sleboda.m98@gmail.com>
2019-12-19 18:20:07 +0100

    second day

---
+++ 11_place.py
@@ -0,0 +1,11 @@
+import tkinter
+
+top = tkinter.Tk()
+
+top.minsize(width=300, height=300)
+top.minsize(width=300, height=300)
+
+button1 = tkinter.Button(top, text='B')
+button1.place(x=40, y=40, anchor="center")
+
+top.mainloop()
